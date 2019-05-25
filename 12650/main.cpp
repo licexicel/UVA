2ae8bac17f870295969d8c4c7fe61d53afcaf922
licexicel <37893175+licexicel@users.noreply.github.com>
2019-05-25 15:53:16 +0800

    Add files via upload

---
+++ 12650/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  12650
+//
+//  Created by 謝盈昊 on 2019/2/22.
+//  Copyright © 2019 謝盈昊. All rights reserved.
+// finish
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int total = 0, live = 0;
+    
+    while(scanf("%d %d",&total,&live) != EOF){
+        int arr[10000]={0};
+        int run = 0;
+        while(run < live){
+            int found = 0;
+            scanf("%d",&found);
+            arr[found] = 1;
+           
+            run++;
+        }
+        if(live == total){
+            printf("%c",'*');
+        }
+        else{
+            int i = 1;
+            for(i = 1; i < total+1;i++ ){
+                
+                if(arr[i] != 1){
+                    printf("%d ",i);
+                }
+            }
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
