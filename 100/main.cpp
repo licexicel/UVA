b23cb94f2523297d5818c8cd14e0b78fcff76f1c
licexicel <37893175+licexicel@users.noreply.github.com>
2019-07-02 21:02:10 +0800

    Add files via upload

---
+++ 100/main.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  100
+//
+//  Created by 謝盈昊 on 2019/7/2.
+//  Copyright © 2019 謝盈昊. All rights reserved.
+//
+
+#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+int cal(int input,int count){
+    while(input != 1){
+        if(input % 2 == 0){
+            input = input /2;
+        }
+        else{
+            input = input * 3 + 1;
+        }
+        count++;
+    }
+    return count;
+}
+int main(int argc, const char * argv[]) {
+    int a , b;
+    while(cin >> a >> b){
+         int i = a,j = b;
+        int count = 1,maxx = 0;
+        if(i > j){
+            swap(i, j);
+        }
+        while(i <= j){
+            maxx = max( cal(i,count), maxx);
+            i++;
+        }
+        cout << a << " " << b << " "<< maxx << endl;
+        
+    }
+    return 0;
+    
+    
+}
