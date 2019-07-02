29c33f6edc7a390571b277a5bce2685c7f5ca19a
licexicel <37893175+licexicel@users.noreply.github.com>
2019-07-02 23:30:07 +0800

    Add files via upload

---
+++ 105/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  105
+//
+//  Created by 謝盈昊 on 2019/7/2.
+//  Copyright © 2019 謝盈昊. All rights reserved.
+//
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int L,R,H;
+    int arr[50105];
+    int max=0;
+    memset(arr, 0, sizeof(arr));
+    while(cin >> L >> H >> R){
+        if( R > max){
+            max = R;
+        }
+        for(int i = L; i < R;i++){
+            if(H > arr[i]){
+                arr[i] = H;
+            }
+        }
+    }
+    bool space = false;
+    for(int i = 1; i <= max ;i++){
+        if(arr[i-1] != arr[i]){
+            if(space){
+                cout << " ";
+            }
+            space = true;
+            cout << i << " " <<arr[i];
+        }
+    }
+    
+    cout << endl;
+    return 0;
+}
